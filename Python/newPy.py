8305ce258531d6d7a806a2f09245a80ec1cf4a11
srp55045 <srp55045@ssoia.com>
2014-06-06 17:28:10 +0530

    Deleted Python/newPy.py

--- Python/newPy.py
+++
@@ -1,7 +0,0 @@
-#Creates new Python source file
-import datetime
-now = datetime.datetime.now()
-today = str(now.month) + '/' + str(now.day) + '/' + str(now.year)
-f = open('./RENAMEME.py', 'w')
-f.write('#!C:\Python27\python.exe\n#Author: Joshua Newton\n#Created: ' + today + '\n')
-f.close()
